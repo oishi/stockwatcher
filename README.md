553be74ea5173951c7f75fc871e08d71585c1836
Yuichi Oishi <oishi@feedtailor.jp>
2023-08-15 18:02:51 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# stockwatcher
+
+# StockWatcher
+
+## Overview
 StockWatcher is an automated tool that monitors stocks prices. Leveraging Google Apps Script and Python, it allows users to view price fluctuations over specified days, providing a straightforward approach to tracking investment trends.
+
+## Installation
+Follow these steps to set up the environment:
+1. Make sure Python 3.x is installed.
+2. Install required libraries: `pip install yfinance pandas requests`
+3. Create a Google Apps Script project and deploy the code.
+4. Set the GAS endpoint URL in the environment variables.
+
+## Usage
+1. Run the script with specified stock codes: `python3 stock_post.py 2914.T,1419.T`
+2. Run the script without arguments for processing all stocks: `python3 stock_post.py`
+
+## Structure
+- **Google Apps Script**: Manages the spreadsheet, imports and formats stock price data.
+- **Python**: Fetches stock price data using yfinance, sends to GAS in JSON format.
+
+## Contribution
+Contributions to the project are welcome! Feel free to report bugs, suggest features, or make a pull request.
+
+## License
+This project is licensed under the Apache License 2.0. For more details, see the [LICENSE](LICENSE) file.
+
+## Contact
+If you encounter any issues or need support, please [contact us](mailto:your-email@example.com).
---
+++ src/python/price_updater.py
@@ -0,0 +1,54 @@
+import yfinance as yf
+import pandas as pd
+import json
+import requests
+import sys
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def fetch_stock_data(tickers, period):
+    stock_data = {ticker: yf.Ticker(ticker).history(period=period) for ticker in tickers}
+    return stock_data
+
+def convert_to_json(stock_data):
+    json_data = {
+        ticker: [
+            {
+                "date": row.Index.strftime("%Y-%m-%d"),
+                "open": row.Open,
+                "high": row.High,
+                "low": row.Low,
+                "close": row.Close
+            }
+            for row in data.itertuples()
+        ]
+        for ticker, data in stock_data.items()
+    }
+    return json_data
+
+def post_to_gas(json_data, gas_url):
+    response = requests.post(gas_url, json=json_data)
+    return response
+
+def fetch_tickers_from_gas(gas_url):
+    response = requests.get(gas_url)
+    return json.loads(response.text)
+
+if __name__ == "__main__":
+    period_input = sys.argv[1] if len(sys.argv) > 1 else "10d"
+
+    gas_url = os.getenv("GAS_ENDPOINT_URL")  # GASエンドポイントURLを環境変数から取得
+
+    if not gas_url:
+        print("Please set the GAS_ENDPOINT_URL in the .env file.")
+        sys.exit(1)
+
+    tickers = fetch_tickers_from_gas(gas_url) if len(sys.argv) <= 1 else sys.argv[1].split(',')
+
+    stock_data = fetch_stock_data(tickers, period_input)
+    json_data = convert_to_json(stock_data)
+
+    response = post_to_gas(json_data, gas_url)
+    print(response.text)
